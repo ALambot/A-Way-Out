bd6fbd8458df408f66c134603ef85d365b31cfc1
hbaudhuin <heloise.baudhuin@student.uclouvain.be>
2018-03-06 10:17:57 +0100

    Amelioration layout cypher roll

--- app/src/main/java/com/dolphin/awayout/cypherRoll_enigme.java
+++ app/src/main/java/com/dolphin/awayout/cypherRoll_enigme.java
@@ -1,13 +1,11 @@
 package com.dolphin.awayout;
 
-import android.graphics.Matrix;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
