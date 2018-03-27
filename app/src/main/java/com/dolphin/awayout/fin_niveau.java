0200a1c90433a347a74c6698682612f649eec4a2
Sophie <sophie.schorochoff@student.uclouvain.be>
2018-03-27 10:14:02 +0200

    Correction de layout

--- app/src/main/java/com/dolphin/awayout/fin_niveau.java
+++ app/src/main/java/com/dolphin/awayout/fin_niveau.java
@@ -4,15 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class fin_niveau extends AppCompatActivity {
-    private Button retourMenu;
+    //private Button retourMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_niveau);
-        retourMenu=findViewById(R.id.retour_menu);
+        //retourMenu=findViewById(R.id.retour_menu);
     }
     public void onButtonChoixNiveauClick(View view){
         Intent intent=new Intent(this, Menu.class);
