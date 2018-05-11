311a690968dc358be06226d8b0e21183912301d4
ALambot <antoine.lambot@student.uclouvain.be>
2018-05-11 13:18:02 +0200

    Chapter Demo (sans les images)

--- app/src/main/java/com/dolphin/awayout/MontreGousset.java
+++ app/src/main/java/com/dolphin/awayout/MontreGousset.java
@@ -2,8 +2,12 @@ package com.dolphin.awayout;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.util.Calendar;
 
 /**
  * Created by antoine on 08.05.18.
@@ -17,22 +21,66 @@ public class MontreGousset extends AppCompatActivity{
     private int H = 6;
     private int M = 0;
 
-    private ListView inventaire;
-    private InventoryAdapt inventoryAdapt;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_montre_gousset_enigme);
 
-        //aiguilleH=findViewById(R.id.TODO);
-        //aiguilleM=findViewById(R.id.TODO);
+        aiguilleH=findViewById(R.id.imageMGH);
+        aiguilleM=findViewById(R.id.imageMGM);
 
-        //inventaire=findViewById(R.id.inventaire_liste); TO CHECK
+        H = (H+12)%12;
+        M = (M+60)%60;
 
-        inventoryAdapt = GameState.getGameState().getInventoryAdapt();
-
-
+        aiguilleH.setRotation(H*360/12);
+        aiguilleM.setRotation(M*360/12);
     }
 
+    public void bHL(View view) {
+        H--;
+        H = (H+12)%12;
+        aiguilleH.setRotation(H*360/12);
+        //Log.d("KEK", "H : " + H + " " + aiguilleH.getRotation());
+    }
+
+    public void bHR(View view) {
+        H++;
+        H = (H+12)%12;
+        aiguilleH.setRotation(H*360/12);
+        //Log.d("KEK", "H : " + H + " " + aiguilleH.getRotation());
+    }
+
+    public void bML(View view) {
+        M--;
+        M = (M+60)%60;
+        aiguilleM.setRotation(M*360/60);
+        //Log.d("KEK", "M : " + M + " " + aiguilleM.getRotation());
+    }
+
+    public void bMR(View view) {
+        M++;
+        M = (M+60)%60;
+        aiguilleM.setRotation(M*360/60);
+        //Log.d("KEK", "M : " + M + " " + aiguilleM.getRotation());
+    }
+
+    public void OK(View view) {
+        Calendar now = Calendar.getInstance();
+        int curH = now.get(Calendar.HOUR);
+        int curM = now.get(Calendar.MINUTE);
+        Log.d("KEK","NOW - H : "+curH+" - M : "+curM);
+
+        int mirrorH = 12 - H;
+        int mirrorM = 60 - M;
+        mirrorH = (mirrorH+12)%12;
+        mirrorM = (mirrorM+60)%60;
+        Log.d("KEK","APP - H : "+mirrorH+" - M : "+mirrorM);
+
+        if(mirrorH == curH && mirrorM == curM){
+            GameState.getGameState().getInteractions().enigmeSuccess("Montre gousset");
+        }
+        else{
+            // TODO
+        }
+    }
 }
